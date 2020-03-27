25597bf48d5a4a4b2612569c68bf856b7d3a3c84
Sam Freeman <sam.freeman.55@gmail.com>
2020-03-27 19:48:11 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# sam-io
+# sam-io v1
 Utilities for paths, files, directories, and console input
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	rootDir: 'src',
+	coverageDirectory: 'test-results',
+	coverageReporters: ["json", "html", "text"]
+}
